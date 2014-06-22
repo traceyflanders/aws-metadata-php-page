371c4b67c1b88e75830d734108d89fea24947132
AlphaMusk <alphamusk@networkpulse.com>
2014-06-21 20:11:43 -0600

    wdc

--- index.php
+++ index.php
@@ -66,8 +66,8 @@ if (empty($_GET['refresh'])) {
 <div id="page-container">
 
 	<div id="main-nav">
-		<span>Page Refresh</span>
 		<ul>
+			<li><span>Page Refresh</span></li>
 			<li><a href="<?php echo $php_self.'?refresh=2'; ?>">2s</a></li>
 			<li><a href="<?php echo $php_self.'?refresh=5'; ?>">5s</a></li>
 			<li><a href="<?php echo $php_self.'?refresh=30'; ?>">30s</a></li>
