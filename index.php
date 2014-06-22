68c90200e39a913fca69b6a80de7c6f676f046b4
AlphaMusk <alphamusk@networkpulse.com>
2014-06-21 19:43:57 -0600

    ZCXZ

--- index.php
+++ index.php
@@ -67,8 +67,7 @@ if (empty($_GET['refresh'])) {
 
 	<div id="main-nav">
 		<ul>
-
-			<li><a href="<?php echo $php_self.'?refresh=2'; ?>">2s</a></li>
+			<li><a href="<?php echo $php_self.'?refresh=2'; ?>">Refresh 2s</a></li>
 			<li><a href="<?php echo $php_self.'?refresh=5'; ?>">5s</a></li>
 			<li><a href="<?php echo $php_self.'?refresh=30'; ?>">30s</a></li>
 			<li><a href="<?php echo $php_self.'?refresh=60'; ?>">60s</a></li>
