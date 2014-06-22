e55b98bb680908c94909b19eca666930b545a65c
AlphaMusk <alphamusk@networkpulse.com>
2014-06-21 21:32:15 -0600

    asd

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if (empty($_GET['refresh'])) {
  function findAZ ($az) {
  	// check if the value is null/empty
  	if (empty($az) || isnull($az) || !isset($az)) {
- 		echo 'unknown az';
+ 		return 'unknown az';
  }
 ?>
 
@@ -96,7 +96,7 @@ if (empty($_GET['refresh'])) {
 			<h2>AWS Region</h2>
 				<p>US-x-x</p>
 			<h3>Availability Zone</h3>
-				<p><?php echo findaz($meta_data['availability-zone']);?></p>
+				<p><?php echo findAZ($meta_data['availability-zone']); ?></p>
 			<br>
 			<h3>Information</h3>
 			<p>Server: <?php echo $server_software.'<br>IP: ';?><a href="http://<?php echo $server_ip; ?>"><?php echo $server_ip; ?></a></p>
